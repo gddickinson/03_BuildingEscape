92f1d0553bc11d7e9a06c1d1fb7b386b23f25f34
George Dickinson <george.dickinson@gmail.com>
2018-06-06 19:33:47 -0600

    Added physics handle with error message

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
+
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -27,5 +29,7 @@ public:
 private:
 	// How far	ahead of the player can we reach in cm
 	float Reach = 100.0f;
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
 	
 };
