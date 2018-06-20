2e46f461b1d3b55deae5b9b4713e5b1e54087221
George Dickinson <george.dickinson@gmail.com>
2018-06-19 19:09:41 -0600

    pointer protection added with initialization to nullptr, if (! XXXX) { return; } loops and error messaging - protects from crashes with pointer errors

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -21,9 +21,11 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
 	Owner = GetOwner();
-	
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing pressure plate!"), *(GetOwner()->GetName()));
+	}
 }
 
 void UOpenDoor::OpenDoor()
@@ -62,6 +64,8 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 
 	// Find all the overlapping actors
 	TArray<AActor*> OverlappingActors;
+
+	if (!PressurePlate) { return TotalMass; }
 	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
 
 	// Iterate through them adding their masses
